a53cf8c38805c10cf2dc0b9be3c31175bd6a04da
Sanek128 <sanek-sp@ukr.net>
2020-10-11 00:50:59 +0300

    HW3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { UserComponent } from './entity/user/components/user/user.component';
+import { RouterModule } from '@angular/router';
+import { AllUsersComponent } from './entity/user/components/all-user/all-user.component';
+import { UserResolveService } from './entity/user/service/user-resolve.service';
+import { PostComponent } from './entity/post/components/post/post/post.component';
+import { AllPostComponent } from './entity/post/components/all-post/all-post/all-post.component';
+import { PostResolveService } from './entity/post/service/post-resolve.service';
+import { CommentComponent } from './entity/comment/components/comment/comment/comment.component';
+import { AllCommentComponent } from './entity/comment/components/all-comment/all-comment/all-comment.component';
+import { CommentResolveService } from './entity/comment/service/comment-resolve.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AllUsersComponent,
+    PostComponent,
+    AllPostComponent,
+    CommentComponent,
+    AllCommentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'users', component: AllUsersComponent, resolve: {xxx: UserResolveService}},
+      {path: 'posts', component: AllPostComponent, resolve: {ppp: PostResolveService}},
+      {path: 'comments', component: AllCommentComponent, resolve: {ccc: CommentResolveService}}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/entity/user/components/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  @Input()
+  user: User;
+
+  constructor() { }
+
+  
+}
